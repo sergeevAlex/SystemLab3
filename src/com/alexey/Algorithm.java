5e681c3366c390c64b6fb9b21cf9eec35cd7a1a5
sergeevAlex <alexsergeev@i.ua>
2016-12-20 18:04:22 +0200

    Ended

--- src/com/alexey/Algorithm.java
+++ src/com/alexey/Algorithm.java
@@ -1,56 +1,70 @@
 package com.alexey;
-
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class Algorithm {
-    String st;
-    int time,memory;
-
-
-
-    public static void optim(List<Comp> mass){
+    public static ArrayList<List<Comp>> optim(List<Comp> mass) {
         int comp = 1024;
-    ArrayList<Comp> fortime = new ArrayList<>();
-    ArrayList<List<Comp>> result = new ArrayList<>();
+        ArrayList<Comp> fortime = new ArrayList<>();
+        ArrayList<List<Comp>> result = new ArrayList<>();
         int counter = 0;
-        while(!mass.isEmpty()){
-        result.add(new ArrayList<>());
-        result.get(counter).add(mass.get(0));
-        int det = comp - mass.get(0).getMemory();
-        for(int i =1;i<mass.size();i++){
-            if(mass.get(i).getMemory() <= det){
-                fortime.add(mass.get(i));
-            }
-        }
-        if(fortime.isEmpty()){
-            mass.remove(0);
-            counter++;
-        }
-        else {
-            Comp vrem = fortime.get(0);
-            for (int i = 1; i < fortime.size(); i++) {
-                if (vrem.getMemory() > fortime.get(i).getMemory()) {
-                    vrem = fortime.get(i);
+        while (!mass.isEmpty()) {
+            result.add(new ArrayList<>());
+            result.get(counter).add(mass.get(0));
+            int det = comp - mass.get(0).getMemory();
+            for (int i = 1; i < mass.size(); i++) {
+                if (mass.get(i).getMemory() <= det) {
+                    fortime.add(mass.get(i));
                 }
             }
-            for (int i = 0; i < mass.size(); i++) {
-                if (vrem.getName().compareTo(mass.get(i).getName()) == 0) {
-                    result.get(counter).add(mass.get(i));
-                    mass.remove(i);
+            if (fortime.isEmpty()) { //Этот процесс можно выполнить только последовательно!
+                mass.remove(0);
+                counter++;
+            } else {
+                Comp vrem = fortime.get(0);
+                for (int i = 1; i < fortime.size(); i++) {
+                    if (vrem.getMemory() > fortime.get(i).getMemory()) {
+                        vrem = fortime.get(i);
+                    }
                 }
+                for (int i = 0; i < mass.size(); i++) {
+                    if (vrem.getName().compareTo(mass.get(i).getName()) == 0) {
+                        result.get(counter).add(mass.get(i));
+                        mass.remove(i);
+                    }
+                }
+
+                while(!fortime.isEmpty()){
+                    det = det - vrem.getMemory();
+                    fortime.clear();
+                    for (int i = 1; i < mass.size(); i++) {
+                        if (mass.get(i).getMemory() <= det) {
+                            fortime.add(mass.get(i));
+                        }
+                    }
+                    if(fortime.isEmpty()) break;
+                    vrem = fortime.get(0);
+                    for (int i = 1; i < fortime.size(); i++) {
+                        if (vrem.getMemory() > fortime.get(i).getMemory()) {
+                            vrem = fortime.get(i);
+                        }
+                    }
+                    for (int i = 0; i < mass.size(); i++) {
+                        if (vrem.getName().compareTo(mass.get(i).getName()) == 0) {
+                            result.get(counter).add(mass.get(i));
+                            mass.remove(i);
+                        }
+                    }
+
+                }
+                fortime.clear();
+                mass.remove(0);
+                counter++;
             }
-            fortime.clear();
-            mass.remove(0);
-            counter++;
-        }
-    }
-        for(int i =0;i<result.size();i++){
-        System.out.println(result.get(i));
-        }
-    }
 
-  
-
+        }
+//        for (int i = 0; i < result.size(); i++) {
+//            System.out.println(result.get(i));
+//        }
+        return result;
+    }
 }
--- src/com/alexey/Main.java
+++ src/com/alexey/Main.java
@@ -1,18 +1,19 @@
 package com.alexey;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import static com.alexey.Algorithm.optim;
-import static com.alexey.Comp.COMPARE_BY_TIME;
 public class Main {
     public static void main(String[] args) {
-        int Max = 700;
-        int Min = 250;
+        final int comp = 1024;
+        int Max = (int) (0.7*comp);
+        int Min = (int) (0.2*comp);
         int timeMin = 10;
         int timeMax = 60;
         final int count = 10;
-        final int comp = 1024;
+        System.out.println("Компьютер со следующими характеристиками: Частота процессора: 2,3 ГГц;Оперативная память " + comp + " Мб");
+        System.out.println("Необходимо последовательно выполнить следующие процессы:");
+        ArrayList<List<Comp>> Answer;
         List<Comp> mass = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             mass.add(new Comp("Process" + i, (Min + (int) (Math.random() * (Max - Min) + 1)), (timeMin + (int) (Math.random() * (timeMax - timeMin) + 1))));
@@ -20,24 +21,40 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println(mass.get(i));
         }
-
         int sum = 0;
         for (int i = 0; i < 10; i++) {
             sum = sum + mass.get(i).getTime();
         }
-        System.out.println("При последовательном выполнении: " + sum + " секунд");
+        System.out.println("Если эти процессы будут выполняться последовательно, то это займет " + sum + " секунд");
         Collections.sort(mass,Comp.COMPARE_BY_TIME);
         System.out.println();
-        for (int i = 0; i < 10; i++) {
-            System.out.println(mass.get(i));
+
+        Answer = optim(mass);
+
+        int generally = 0;
+        for(int i = 0;i<Answer.size();i++){
+
+            System.out.println("За " +  FindMax(Answer.get(i)) + " сек. будут параллельно " +
+                    "выполнены следующие процессы: ");
+           for(int j =0;j<Answer.get(i).size();j++){
+                System.out.printf("%s, ", Answer.get(i).get(j).getName());
+            }
+            generally += FindMax(Answer.get(i));
+            System.out.println();
+
         }
-        int temp = comp;
-        for(int i =0;i<10;i++){
-            if(  mass.get(i).getMemory()< temp)
-                temp = mass.get(i).getMemory();
+        System.out.println("Общее время при параллельном выполнении: " + generally + " сек.");
+        System.out.println("Разница по времени: " + (sum - generally) + " сек." );
+    }
+        public static int FindMax(List<Comp> arr){
+            int max = arr.get(0).getTime();
+            for(int i =0;i < arr.size();i++){
+                if(arr.get(i).getTime() > max){
+                    max = arr.get(i).getTime();
+                }
+            }
+            return max;
         }
-        int minMemory = temp;
-        System.out.println("Min memory: " + minMemory);
-        optim(mass);
-        }
+
+        
     }
